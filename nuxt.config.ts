94be52302e538c5b192a600e513c62b1a5fee2a9
droplet <8845083+droplet-js@users.noreply.github.com>
2024-06-17 16:24:04 +0800

    fx

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {},
   runtimeConfig: {},
-  devtools: { enabled: true }
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      titleTemplate: '%s - Nuxt.js',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
+  },
+  routeRules: {},
+  devtools: { enabled: true },
 })
