9e29d4ea8d4d146ecf9b4de44f88ede1267fbe22
droplet <8845083+droplet-js@users.noreply.github.com>
2024-06-26 21:19:58 +0800

    fx

--- app/app.vue
+++ app/app.vue
@@ -5,12 +5,13 @@
   </div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
+// useHead({});
+// useSeoMeta({});
 
 const appConfig = useAppConfig();
 console.log(appConfig.theme);
 
-const resp = await useFetch('/api');
+const resp = await useFetch("/api");
 console.log(`resp: ${resp.status.value} - ${resp.data.value}`);
-
 </script>
--- server/api/index.ts
+++ server/api/index.ts
@@ -1,1 +1,1 @@
-export default defineEventHandler(() => 'Nuxt API');
+export default defineEventHandler(() => "Nuxt API");
