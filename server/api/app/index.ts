747e0b2923e024733db7c694d9e02a4181e7f2b8
droplet <8845083+droplet-js@users.noreply.github.com>
2024-06-27 17:41:02 +0800

    fx

--- server/api/app/index.ts
+++ server/api/app/index.ts
@@ -1,4 +1,6 @@
 import { z } from "zod";
+import * as appstore from "app-store-scraper";
+import * as googleplay from "google-play-scraper";
 
 // const resp = await useFetch("/api/app?platform=android&package_name=com.example.app");
 export default defineEventHandler(async (event) => {
@@ -7,10 +9,56 @@ export default defineEventHandler(async (event) => {
     z.object({
       platform: z.enum(["android", "ios"]),
       package_name: z.string(),
+      country: z.string().optional(),
+      lang: z.string().optional(),
     }).parse,
   );
   console.log(`Query: ${JSON.stringify(query)}`);
-  await event.respondWith(Response.json({ msg: "Nuxt API" }));
+  let data:
+    | {
+        id?: number;
+        appId: string;
+        title: string;
+        icon: string;
+        description: string;
+        version: string;
+        url: string;
+      }
+    | undefined;
+  switch (query.platform) {
+    case "android":
+      const androidInfo = await googleplay.app({
+        appId: query.package_name,
+        country: query.country ?? "us",
+        lang: query.lang ?? "en",
+      });
+      data = {
+        appId: androidInfo.appId,
+        title: androidInfo.title,
+        icon: androidInfo.icon,
+        description: androidInfo.description,
+        version: androidInfo.version,
+        url: androidInfo.url,
+      };
+      break;
+    case "ios":
+      const iosInfo = await appstore.app({
+        id: query.package_name,
+        country: query.country ?? "us",
+        lang: query.lang ?? "en",
+      });
+      data = {
+        id: iosInfo.id,
+        appId: iosInfo.appId,
+        title: iosInfo.title,
+        icon: iosInfo.icon,
+        description: iosInfo.description,
+        version: iosInfo.version,
+        url: iosInfo.url,
+      };
+      break;
+  }
+  await event.respondWith(Response.json({ data: data }));
   // return {
   //     msg: "Nuxt API",
   // };
