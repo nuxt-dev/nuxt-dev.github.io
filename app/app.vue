2107f158ec440f3150a3c6c951f3455863322316
droplet <8845083+droplet-js@users.noreply.github.com>
2024-06-27 17:48:10 +0800

    fx

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,37 @@
 <template>
   <div>
+    <!-- <NuxtRouteAnnouncer /> -->
+    <!-- <NuxtWelcome /> -->
+
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+// useHead({});
+// useSeoMeta({});
+
+const appConfig = useAppConfig();
+console.log(appConfig.theme);
+
+const apiResp = await useFetch("/api");
+console.log(
+  `apiResp: ${apiResp.status.value} - ${JSON.stringify(apiResp.data.value)}`,
+);
+
+const androidAppResp = await useFetch(
+  "/api/app?platform=android&package_name=com.android.chrome",
+);
+console.log(
+  `appResp: ${androidAppResp.status.value} - ${JSON.stringify(androidAppResp.data.value)}`,
+);
+
+const iosAppResp = await useFetch(
+  "/api/app?platform=ios&package_name=com.google.chrome.ios",
+);
+console.log(
+  `appResp: ${iosAppResp.status.value} - ${JSON.stringify(iosAppResp.data.value)}`,
+);
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,5 +2,8 @@
   <div>
     <h1>Welcome to the homepage</h1>
     <AppAlert> This is an auto-imported component </AppAlert>
+    <NuxtImg
+      src="https://www.baidu.com/img/doodlegaokaokaohou_5b0f886d182bfd1761854cb4dfa72fa8.gif"
+    />
   </div>
 </template>
