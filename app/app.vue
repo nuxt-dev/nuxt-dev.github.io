f9f64adb5880783a16045a81918fa182e14629a6
droplet <8845083+droplet-js@users.noreply.github.com>
2024-06-28 20:09:14 +0800

    fx

--- app/app.vue
+++ app/app.vue
@@ -21,17 +21,21 @@ console.log(
   `apiResp: ${apiResp.status.value} - ${JSON.stringify(apiResp.data.value)}`,
 );
 
-const androidAppResp = await useFetch(
-  "/api/app?platform=android&package_name=com.android.chrome",
-);
-console.log(
-  `Android App Resp: ${androidAppResp.status.value} - ${JSON.stringify(androidAppResp.data.value)}`,
-);
+// const androidAppResp = await useFetch(
+//   "/api/app?platform=android&package_name=com.android.chrome",
+// );
+// console.log(
+//   `Android App Resp: ${androidAppResp.status.value} - ${JSON.stringify(androidAppResp.data.value)}`,
+// );
 
-const iosAppResp = await useFetch(
-  "/api/app?platform=ios&bundle_id=com.google.chrome.ios",
-);
-console.log(
-  `iOS App Resp: ${iosAppResp.status.value} - ${JSON.stringify(iosAppResp.data.value)}`,
-);
+// const iosAppResp = await useFetch(
+//   "/api/app?platform=ios&bundle_id=com.google.chrome.ios",
+// );
+// console.log(
+//   `iOS App Resp: ${iosAppResp.status.value} - ${JSON.stringify(iosAppResp.data.value)}`,
+// );
+
+const appsResp = await useFetch("/api/app/list");
+console.log(`Apps Resp: ${appsResp.status.value} - ${JSON.stringify(appsResp.data.value)}`);
+
 </script>
