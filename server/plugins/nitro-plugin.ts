a21ea7d1519da69f954f298e503b1b1fb609c18e
droplet <8845083+droplet-js@users.noreply.github.com>
2024-06-28 17:30:14 +0800

    fx

--- server/plugins/nitro-plugin.ts
+++ server/plugins/nitro-plugin.ts
@@ -2,7 +2,7 @@ export default defineNitroPlugin((nitro) => {
   nitro.hooks.hook("render:html", (html, { event }) => {
     console.log("render:html");
     // console.log("render:html", html);
-    html.bodyAppend.push("<hr>Appended by custom plugin</hr>");
+    // html.bodyAppend.push("<hr>Appended by custom plugin</hr>");
   });
 
   nitro.hooks.hook("render:response", (response, { event }) => {
