0f12a1fba8f044f4139ca45c6e098a1ff4edc8bb
droplet <8845083+droplet-js@users.noreply.github.com>
2024-07-02 19:58:28 +0800

    fx

--- app/pages/app/[id].vue
+++ app/pages/app/[id].vue
@@ -4,4 +4,7 @@
 
 const route = useRoute();
 console.log(route.params.id);
+
+const appResp = await useFetch(`/api/app/info?id=${route.params.id}`);
+console.log(`App Resp: ${appResp.status.value} - ${JSON.stringify(appResp.data.value)}`);
 </script>
