af496d6423498bfa5947bc1ae75452f66a659e58
droplet <8845083+droplet-js@users.noreply.github.com>
2024-07-02 20:51:10 +0800

    fx

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,18 +2,20 @@
   <div>
     <h1>Welcome to the homepage</h1>
     <AppAlert> This is an auto-imported component </AppAlert>
+    <br/>
     <NuxtImg src="https://www.baidu.com/img/doodlegaokaokaohou_5b0f886d182bfd1761854cb4dfa72fa8.gif" />
+    <br/>
     <NuxtLink 
       v-for="app in apps" 
       :key="app.id"
       :to="`/app/${app.id}`"
       >
-      {{ app.title }}
+      {{ app.title }}<br/>
     </NuxtLink>
   </div>
 </template>
 
 <script setup lang="ts">
 const { data: apps } = await useFetch("/api/app/list");
-console.log(`Apps: ${JSON.stringify(apps.value)}`);
+// console.log(`Apps: ${JSON.stringify(apps.value)}`);
 </script>
