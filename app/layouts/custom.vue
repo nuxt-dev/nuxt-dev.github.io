cba9237ea9a9044f5f14c7212dde46be775aa9a1
droplet <8845083+droplet-js@users.noreply.github.com>
2024-07-04 18:56:06 +0800

    fx

--- app/layouts/custom.vue
+++ app/layouts/custom.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>Default</p>
+    <p>Custom</p>
     <AppHeader />
     <slot />
     <AppFooter />
--- app/pages/about.vue
+++ app/pages/about.vue
@@ -3,3 +3,9 @@
     <p>This page will be displayed at the /about route.</p>
   </section>
 </template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "custom",
+});
+</script>
