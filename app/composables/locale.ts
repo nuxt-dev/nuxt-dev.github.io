4e70c7fc7cd8a152638ed433687c3faba295f176
droplet <8845083+droplet-js@users.noreply.github.com>
2024-07-05 19:09:52 +0800

    fx

--- app/composables/locale.ts
+++ app/composables/locale.ts
@@ -4,13 +4,13 @@ export const useLocale = () => useState<string>("locale", () => useDefaultLocale
 
 export const useDefaultLocale = (fallback = "en-US") => {
   const locale = ref(fallback);
-  if (process.server) {
+  if (import.meta.server) {
     // Learn more about the nuxtApp interface on https://nuxt.com/docs/guide/going-further/internals#the-nuxtapp-interface
     const reqLocale = useRequestHeaders()["accept-language"]?.split(",")[0];
     if (reqLocale) {
       locale.value = reqLocale;
     }
-  } else if (process.client) {
+  } else if (import.meta.client) {
     const navLang = navigator.language;
     if (navLang) {
       locale.value = navLang;
